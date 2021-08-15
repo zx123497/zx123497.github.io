e29cb38955f916736abbeafffeb90500d17d7199
leo <leo000111444@gmail.com>
2021-08-16 01:01:45 +0800

    :lipstick: add style in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Img from "../assets/img.JPG";
-import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Hana from "../assets/hana.png";
 import Family from "../assets/family.png";
 import Mise from "../assets/mise.png";
+import UX from "../assets/ux.svg";
+import Engineer from "../assets/engineer.svg";
+import Paint from "../assets/paint-palette.svg";
+import Cir1 from "../assets/cir1.svg";
+import Cir2 from "../assets/cir2.svg";
 const useStyles = makeStyles({
   root: {
     width: "100%",
@@ -27,6 +33,68 @@ const useStyles = makeStyles({
   },
 });
 
+function FadeInWhenVisible({ children, delay }) {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("show");
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      transition={{ duration: 0.3 }}
+      variants={{
+        whileHover: { scale: 1.1 },
+        hidden: { opacity: 0, scale: 0, y: 300 },
+        show: {
+          opacity: 1,
+          scale: 1,
+          y: 0,
+          transition: { duration: 1, ease: "backOut", delay: delay },
+        },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+function FadeInFireWork({ children, delay }) {
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("show");
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      animate={controls}
+      initial="hidden"
+      transition={{ duration: 0.3 }}
+      variants={{
+        hidden: { opacity: 0, scale: 0 },
+        show: {
+          opacity: 1,
+          scale: 1,
+
+          transition: { duration: 0.75, ease: "backOut", delay: 0.75 },
+        },
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
 const Home = () => {
   const ref = useRef(null);
   const scrollToNext = () => {
@@ -37,7 +105,7 @@ const Home = () => {
   var a = 20 + "as";
   return (
     <motion.div
-      initial={{ opacity: 0, y: -100 }}
+      initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 1 }}
       exit={{
@@ -132,7 +200,7 @@ const Home = () => {
         <div
           id="2"
           style={{
-            backgroundColor: "#ddd",
+            backgroundColor: "#1B3447",
             display: "flex",
             position: "relative",
             justifyContent: "center",
@@ -142,6 +210,16 @@ const Home = () => {
           }}
           ref={ref}
         >
+          <img
+            src={Mise}
+            style={{
+              position: "absolute",
+              bottom: -15,
+              width: "22%",
+              left: 0,
+              filter: "drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.3))",
+            }}
+          />
           <img
             src={Hana}
             style={{
@@ -183,8 +261,51 @@ const Home = () => {
               filter: "drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.3))",
             }}
           />
-          <div>
-            <h2 style={{ marginBottom: "3rem" }}>我的專長</h2>
+          <img
+            src={Cir1}
+            style={{
+              position: "absolute",
+              top: "11rem",
+              left: "10rem",
+              width: "10rem",
+              filter: "drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.3))",
+            }}
+          />
+
+          <img
+            src={Cir2}
+            style={{
+              position: "absolute",
+              top: "6rem",
+              left: "15rem",
+              width: "10rem",
+              filter: "drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.3))",
+            }}
+          />
+          <img
+            src={Cir2}
+            style={{
+              position: "absolute",
+              top: "6rem",
+              right: "20rem",
+              width: "20rem",
+              filter: "drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.3))",
+            }}
+          />
+          <img
+            src={Cir1}
+            style={{
+              position: "absolute",
+              bottom: "5rem",
+              right: "5rem",
+              width: "20rem",
+              filter: "drop-shadow(0px 0px 6px rgba(0, 0, 0, 0.3))",
+            }}
+          />
+          <div style={{ position: "relative" }}>
+            <h2 style={{ marginBottom: "3rem", color: "#FFF" }}>
+              我的專長 My Service
+            </h2>
             <motion.div
               style={{
                 display: "flex",
@@ -195,70 +316,98 @@ const Home = () => {
                 show: {
                   opacity: 1,
                   y: 0,
-                  transition: { staggerChildren: 0.5 },
                 },
               }}
               initial="hidden"
               animate="show"
             >
-              <motion.div
-                variants={{
-                  whileHover: { scale: 1.1 },
-                  hidden: { opacity: 0, scale: 0, y: 300 },
-                  show: {
-                    opacity: 1,
-                    scale: 1,
-                    y: 0,
-                    transition: { duration: 1, ease: "backOut" },
-                  },
-                }}
-                style={{
-                  width: "25rem",
-                  height: "20rem",
-                  margin: "0 2rem",
-                  borderRadius: "10px",
-                  boxShadow: "3px 3px 6px rgba(0,0,0,0.2)",
-                  backgroundColor: "#FFF",
-                }}
-              ></motion.div>
-              <motion.div
-                variants={{
-                  hidden: { opacity: 0, scale: 0, y: 300 },
-                  show: {
-                    opacity: 1,
-                    scale: 1,
-                    y: 0,
-                    transition: { duration: 1, ease: "backOut" },
-                  },
-                }}
-                style={{
-                  width: "25rem",
-                  height: "20rem",
-                  margin: "0 2rem",
-                  borderRadius: "10px",
-                  boxShadow: "3px 3px 6px rgba(0,0,0,0.2)",
-                  backgroundColor: "#FFF",
-                }}
-              ></motion.div>
-              <motion.div
-                variants={{
-                  hidden: { opacity: 0, scale: 0, y: 300 },
-                  show: {
-                    opacity: 1,
-                    scale: 1,
-                    y: 0,
-                    transition: { duration: 1, ease: "backOut" },
-                  },
-                }}
-                style={{
-                  width: "25rem",
-                  height: "20rem",
-                  margin: "0 2rem",
-                  borderRadius: "10px",
-                  boxShadow: "3px 3px 6px rgba(0,0,0,0.2)",
-                  backgroundColor: "#FFF",
-                }}
-              ></motion.div>
+              <FadeInWhenVisible delay={0.25}>
+                <div
+                  style={{
+                    width: "25rem",
+                    height: "20rem",
+                    margin: "0 2rem",
+                    borderRadius: "10px",
+                    boxShadow: "3px 3px 6px rgba(0,0,0,0.2)",
+                    backgroundColor: "rgba(255,255,255,.95)",
+                    padding: "2rem",
+                  }}
+                >
+                  <div>
+                    <img
+                      src={UX}
+                      style={{ width: "5rem", marginBottom: "2rem" }}
+                    />
+                  </div>
+                  <div>
+                    <h3>UI/UX 設計</h3>
+                  </div>
+                  <div>
+                    <h5 style={{ color: "#999", lineHeight: "1.5rem" }}>
+                      熟悉的UI/UX設計軟體為Adobe
+                      XD，喜歡上Pinterest等圖片庫尋找靈感、學習排版，平常也會畫畫，然後把自己的作品隱藏在網頁中當作彩蛋
+                    </h5>
+                  </div>
+                </div>
+              </FadeInWhenVisible>
+              <FadeInWhenVisible delay={0.5}>
+                <div
+                  style={{
+                    width: "25rem",
+                    height: "20rem",
+                    margin: "0 2rem",
+                    borderRadius: "10px",
+                    boxShadow: "3px 3px 6px rgba(0,0,0,0.2)",
+                    backgroundColor: "rgba(255,255,255,.95)",
+                    padding: "2rem",
+                  }}
+                >
+                  <div>
+                    <img
+                      src={Engineer}
+                      style={{ width: "5rem", marginBottom: "2rem" }}
+                    />
+                  </div>
+                  <div>
+                    <h3>前端開發</h3>
+                  </div>
+                  <div>
+                    <h5 style={{ color: "#999", lineHeight: "1.5rem" }}>
+                      熟悉的前端框架為React、React native，最喜歡的套件是Framer
+                      Motion，他是個很厲害的動畫套件，在大學就接觸滿多前端的任務
+                    </h5>
+                  </div>
+                </div>
+              </FadeInWhenVisible>
+              <FadeInWhenVisible delay={0.75}>
+                <div
+                  style={{
+                    width: "25rem",
+                    height: "20rem",
+                    margin: "0 2rem",
+                    borderRadius: "10px",
+                    boxShadow: "3px 3px 6px rgba(0,0,0,0.2)",
+                    backgroundColor: "rgba(255,255,255,.95)",
+                    padding: "2rem",
+                  }}
+                >
+                  <div>
+                    <img
+                      src={Paint}
+                      style={{ width: "5rem", marginBottom: "2rem" }}
+                    />
+                  </div>
+                  <div>
+                    <h3>美術設計</h3>
+                  </div>
+                  <div>
+                    <h5 style={{ color: "#999", lineHeight: "1.5rem" }}>
+                      會使用 Adobe Photoshop、Adobe
+                      Aftereffect、Procreate，來設計圖、手繪繪圖、動態GIF，平常也喜歡隨手塗鴉
+                    </h5>
+                  </div>
+                </div>
+              </FadeInWhenVisible>
             </motion.div>
           </div>
         </div>
